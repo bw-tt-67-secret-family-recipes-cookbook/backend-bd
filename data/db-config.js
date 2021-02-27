7c0b59ab555fadb59c005efebf3ec6d3ab85ce9b
bdiaz12345 <bdiazmusic5@gmail.com>
2021-02-27 13:12:35 -0500

    fingers crossed

--- data/db-config.js
+++ data/db-config.js
@@ -1,4 +1,6 @@
 const knex = require("knex");
-const configs = require("../knexfile");
+const { env } = require("process");
+const knexfile = require("../knexfile");
 const environment = process.env.NODE_ENV || "development";
-module.exports = knex(configs[environment]);
+const configOptions = knexfile[environment]
+module.exports = knex(configOptions);
